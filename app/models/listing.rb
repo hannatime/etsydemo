5bfca60be4a63b01747c301291df6afe573c04ff
Benjamin Hanna <benhanna@Benjamins-MacBook-Pro.local>
2015-08-22 21:22:44 +1000

    fix aws

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,10 +2,12 @@ class Listing < ActiveRecord::Base
   if Rails.env.development?
     has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg"
   else
-    has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg",
-                      :storage => :dropbox,
-                      :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
-                      :path => ":style/:id_:filename"
+    has_attached_file :image,
+    :styles => { :thumbnail => "100x100>" },
+    :storage => :s3,
+    :s3_credentials => "#{Rails.root}/config/aws.yml",
+    :bucket => "etsy-demo99";
+           
   end
 
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
